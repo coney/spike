5ff0736ed8e1ffbad8e09015fc9b90c35ac532a3
Coney Wu <coney.wu@gmail.com>
2014-03-31 19:23:18 +0800

    finish websocket spike

--- websocket/wsapp.rb
+++ websocket/wsapp.rb
@@ -1,5 +1,28 @@
+require 'em-websocket'
 require 'sinatra'
+require 'sinatra-websocket'
+require 'awesome_print'
+
+set :server, 'thin'
+set :sockets, []
 
 get '/' do
-  "Hello World!"
+  if !request.websocket?
+    erb :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        ap msg
+        EM.next_tick { settings.sockets.each { |s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+  end
 end
